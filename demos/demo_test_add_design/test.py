c97cd4df92c71371a24d71b1eef4bdf2d6af0830
Stefan Abi-Karam <stefanabikaram@gmail.com>
2025-04-01 20:05:02 -0400

    fixups

---
+++ demos/demo_test_add_design/test.py
@@ -0,0 +1,114 @@
+from pathlib import Path
+
+from hlsfactory.datasets_builtin import (
+    datasets_builder,
+)
+from hlsfactory.flow_vitis import (
+    VitisHLSImplFlow,
+    VitisHLSImplReportFlow,
+    VitisHLSSynthFlow,
+)
+from hlsfactory.framework import (
+    Design,
+    DesignDataset,
+    DesignDatasetCollection,
+    count_total_designs_in_dataset_collection,
+)
+from hlsfactory.opt_dsl_frontend import OptDSLFrontend
+from hlsfactory.utils import (
+    DirSource,
+    ToolPathsSource,
+    get_tool_paths,
+    get_work_dir,
+    remove_and_make_new_dir_if_exists,
+)
+
+WORK_DIR_TOP = get_work_dir(dir_source=DirSource.ENVFILE)
+WORK_DIR = WORK_DIR_TOP / "demo_test_add_design"
+remove_and_make_new_dir_if_exists(WORK_DIR)
+print(f"WORK_DIR: {WORK_DIR}")
+
+N_JOBS = 32
+CPU_AFFINITY = list(range(N_JOBS))
+
+VITIS_HLS_PATH, VIVADO_PATH = get_tool_paths(tool_paths_source=ToolPathsSource.ENVFILE)
+VIVADO_BIN = VIVADO_PATH / "bin" / "vivado"
+VITIS_HLS_BIN = VITIS_HLS_PATH / "bin" / "vitis_hls"
+
+
+DIR_CURRENT = Path(__file__).parent
+
+design_name = "stefan_test_design"
+
+design_to_test_dir = (
+    DIR_CURRENT.parent.parent
+    / "hlsfactory"
+    / "hls_dataset_sources"
+    / "class_ece8893_spring_2025"
+    / design_name
+)
+print(f"design_to_test_dir: {design_to_test_dir}")
+assert design_to_test_dir.exists()
+
+my_design = Design(design_name, design_to_test_dir)
+my_design = my_design.copy_to_new_parent_dir(WORK_DIR)
+
+dataset_dir = WORK_DIR / "my_dataset_with_one_design"
+
+my_dataset_with_one_design = DesignDataset(
+    "my_dataset_with_one_design",
+    dataset_dir,
+    designs=[],
+)
+
+my_dataset_with_one_design.add_design_copy(my_design)
+
+datasets: DesignDatasetCollection = {
+    "my_dataset_with_several_designs": my_dataset_with_one_design,
+}
+
+TIMEOUT_HLS_SYNTH = 60.0 * 8  # 8 minutes
+TIMEOUT_HLS_IMPL = 60.0 * 30  # 30 minutes
+
+toolflow_vitis_hls_synth = VitisHLSSynthFlow(
+    vitis_hls_bin=str(VITIS_HLS_BIN),
+    env_var_xilinx_hls=str(VITIS_HLS_PATH),
+    env_var_xilinx_vivado=str(VIVADO_PATH),
+)
+datasets_post_hls_synth = (
+    toolflow_vitis_hls_synth.execute_multiple_design_datasets_fine_grained_parallel(
+        datasets,
+        False,
+        n_jobs=N_JOBS,
+        cpu_affinity=CPU_AFFINITY,
+        timeout=TIMEOUT_HLS_SYNTH,
+    )
+)
+
+
+toolflow_vitis_hls_implementation = VitisHLSImplFlow(
+    vitis_hls_bin=str(VITIS_HLS_BIN),
+    env_var_xilinx_hls=str(VITIS_HLS_PATH),
+    env_var_xilinx_vivado=str(VIVADO_PATH),
+)
+datasets_post_hls_implementation = toolflow_vitis_hls_implementation.execute_multiple_design_datasets_fine_grained_parallel(
+    datasets_post_hls_synth,
+    False,
+    n_jobs=N_JOBS,
+    cpu_affinity=CPU_AFFINITY,
+    timeout=TIMEOUT_HLS_IMPL,
+)
+
+
+toolflow_vitis_hls_impl_report = VitisHLSImplReportFlow(
+    vitis_hls_bin=str(VITIS_HLS_BIN),
+    vivado_bin=str(VIVADO_BIN),
+    env_var_xilinx_hls=str(VITIS_HLS_PATH),
+    env_var_xilinx_vivado=str(VIVADO_PATH),
+)
+toolflow_vitis_hls_impl_report.execute_multiple_design_datasets_fine_grained_parallel(
+    datasets_post_hls_implementation,
+    False,
+    n_jobs=N_JOBS,
+    cpu_affinity=CPU_AFFINITY,
+)
--- hlsfactory/datasets_builtin.py
+++ hlsfactory/datasets_builtin.py
@@ -15,11 +15,6 @@ DIR_DATASET_PP4FPGAS = HLS_DATASET_DIR / "pp4fpgas"
 DIR_DATASET_VITIS_EXAMPLES = HLS_DATASET_DIR / "vitis_examples"
 DIR_DATASET_ACCELERATORS = HLS_DATASET_DIR / "accelerators"
 
-DIR_DATASET_ECE8893_LAB_1 = HLS_DATASET_DIR / "ece8893_dataset_lab_1"
-DIR_DATASET_Ayush_Saxena_ECE_8893_SPRING_26 = HLS_DATASET_DIR / "ayush_saxena_ece_8893_spring_26"
-DIR_DATASET_Anuvab_Sen_ECE_8893_SPRING_29 = HLS_DATASET_DIR / "anuvab_sen_ece_8893_spring_29"
-DIR_DATASET_MATRIX_METHODS = HLS_DATASET_DIR / "matrix_methods"
-
 
 DIR_ALL = [
     DIR_DATASET_POLYBENCH,
@@ -28,7 +23,6 @@ DIR_ALL = [
     DIR_DATASET_PP4FPGAS,
     DIR_DATASET_VITIS_EXAMPLES,
     DIR_DATASET_ACCELERATORS,
-    DIR_DATASET_ECE8893_LAB_1
 ]
 
 
@@ -37,12 +31,6 @@ def check_dataset_dir_exists(dir_path: Path) -> None:
         msg = f"Dataset directory not found: {dir_path}"
         raise FileNotFoundError(msg)
 
-
-def dataset_ece8893_lab_1_builder(name: str, work_dir: Path) -> DesignDataset:
-    check_dataset_dir_exists(DIR_DATASET_ECE8893_LAB_1)
-    new_dir = work_dir / DIR_DATASET_ECE8893_LAB_1.name
-    shutil.copytree(DIR_DATASET_ECE8893_LAB_1, new_dir)
-    return DesignDataset.from_dir(name, new_dir)
 
 def dataset_polybench_builder(name: str, work_dir: Path) -> DesignDataset:
     check_dataset_dir_exists(DIR_DATASET_POLYBENCH)
@@ -89,26 +77,6 @@ def dataset_accelerators_builder(name: str, work_dir: Path) -> DesignDataset:
     shutil.copytree(DIR_DATASET_ACCELERATORS, new_dir)
     return DesignDataset.from_dir(name, new_dir)
 
-def dataset_dataset_ayush_saxena_ece_8893_spring_26_builder_builder(name: str, work_dir: Path) -> DesignDataset:
-    check_dataset_dir_exists(DIR_DATASET_Ayush_Saxena_ECE_8893_SPRING_26)
-    new_dir = work_dir / name
-    shutil.copytree(DIR_DATASET_ACCELERATORS, new_dir)
-    return DesignDataset.from_dir(name, new_dir)
-
-
-def dataset_dataset_ayush_saxena_ece_8893_spring_26_builder_builder(name: str, work_dir: Path) -> DesignDataset:
-    check_dataset_dir_exists(DIR_DATASET_Anuvab_Sen_ECE_8893_SPRING_29)
-    new_dir = work_dir / name
-    shutil.copytree(DIR_DATASET_ACCELERATORS, new_dir)
-    return DesignDataset.from_dir(name, new_dir)
-
-
-def dataset_matrix_methods_builder(name: str, work_dir: Path) -> DesignDataset:
-    check_dataset_dir_exists(DIR_DATASET_MATRIX_METHODS)
-    new_dir = work_dir / name
-    shutil.copytree(DIR_DATASET_MATRIX_METHODS, new_dir)
-    return DesignDataset.from_dir(name, new_dir)
-
 
 DATASET_STR_MAP = {
     "polybench": dataset_polybench_builder,
@@ -117,12 +85,6 @@ DATASET_STR_MAP = {
     "pp4fpgas": dataset_pp4fpgas_builder,
     "vitis_examples": dataset_vitis_examples_builder,
     "accelerators": dataset_accelerators_builder,
-
-  
-    "ece8893_lab_1": dataset_ece8893_lab_1_builder,
-    "ayush_saxena_ece_8893_spring_26": dataset_dataset_ayush_saxena_ece_8893_spring_26_builder_builder
-    "matrix_methods": dataset_matrix_methods_builder
-
 }
 
 
@@ -169,5 +131,4 @@ def datasets_all_builder(work_dir: Path) -> DesignDatasetCollection:
         dataset_pp4fpgas.name: dataset_pp4fpgas,
         dataset_vitis_examples.name: dataset_vitis_examples,
         dataset_accelerators.name: dataset_accelerators,
-        "anuvab_sen_ece_8893_spring_29": dataset_dataset_anuvab_sen_ece_8893_spring_29_builder_builder,
     }
