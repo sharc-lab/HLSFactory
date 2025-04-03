90e0a20296bf84f6085423f98370e3f0318130b3
Zhang <mzhang745@ece-linlabsrv01.ece.gatech.edu>
2025-04-03 16:45:59 -0400

    MZ_lab2_lab3

---
+++ hlsfactory/hls_dataset_sources/class_ece8893_spring_2025/Mingyu_Zhang_lab2/dataset_hls_ip_export.tcl
@@ -0,0 +1,11 @@
+open_project project_1
+open_solution "solution1"
+
+# C/RTL co-simulation; can be commented if not needed
+ cosim_design
+
+# export generated RTL as an IP; can be commented if not needed
+export_design -format ip_catalog -flow impl
+
+exit
+
--- hlsfactory/hls_dataset_sources/class_ece8893_spring_2025/Mingyu_Zhang_lab3/dataset_hls.tcl
+++ hlsfactory/hls_dataset_sources/class_ece8893_spring_2025/Mingyu_Zhang_lab3/dataset_hls.tcl
@@ -13,12 +13,12 @@ add_files -tb host.cpp
 add_files -tb A_matrix_csr_sparsity_0.10.bin
 add_files -tb B_matrix_csc_sparsity_0.10.bin
 add_files -tb C_matrix_result_sparsity_0.10.bin
-#add_files -tb A_matrix_csr_sparsity_0.50.bin
-#add_files -tb B_matrix_csc_sparsity_0.50.bin
-#add_files -tb C_matrix_result_sparsity_0.50.bin
-#add_files -tb A_matrix_csr_sparsity_0.80.bin
-#add_files -tb B_matrix_csc_sparsity_0.80.bin
-#add_files -tb C_matrix_result_sparsity_0.80.bin
+add_files -tb A_matrix_csr_sparsity_0.50.bin
+add_files -tb B_matrix_csc_sparsity_0.50.bin
+add_files -tb C_matrix_result_sparsity_0.50.bin
+add_files -tb A_matrix_csr_sparsity_0.80.bin
+add_files -tb B_matrix_csc_sparsity_0.80.bin
+add_files -tb C_matrix_result_sparsity_0.80.bin
 
 open_solution "solution1"
 
@@ -29,15 +29,6 @@ set_part {xczu3eg-sbva484-1-e}
 #create_clock -period 4 -name default
 
 # C synthesis for HLS design, generating RTL
-#csynth_design
-csim_design
-
-# C/RTL co-simulation; can be commented if not needed
-#cosim_design
-
-# export generated RTL as an IP; can be commented if not needed
-#export_design -format ip_catalog -flow impl
-
-
+csynth_design
 
 exit
