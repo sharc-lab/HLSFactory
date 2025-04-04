90878a65fdfd76bddcdc3a5367d41dcb9950803b
Gaurav Singh Chandrabhan <60702055+Gauravchandrabhan@users.noreply.github.com>
2025-04-03 20:32:26 -0400

    Submission of Lab2 HLS design - Gaurav Singh Chandrabhan

---
+++ hlsfactory/hls_dataset_sources/class_ece8893_spring_2025/Gaurav_Singh_Chandrabhan_ece8893_spring25_lab2/dataset_hls.tcl
@@ -0,0 +1,25 @@
+open_project Gaurav_Singh_Chandrabhan_lab-2_design_proj
+
+# add source files
+add_files top.cpp
+add_files -tb host.cpp
+add_files -tb Q_tensor.bin
+add_files -tb V_tensor.bin
+add_files -tb K_tensor.bin
+add_files -tb Output_tensor.bin
+
+open_solution "lab2_Gaurav_Singh_Chandrabhan_spring_2025"
+
+# set top function of the HLS design
+set_top compute_attention_HLS
+
+# FPGA part and clock configuration
+set_part {xczu3eg-sbva484-1-e}
+
+# default frequency is 100 MHz
+create_clock -period 2 -name clk
+
+# C synthesis for HLS design, generating RTL
+csynth_design
+
+exit
