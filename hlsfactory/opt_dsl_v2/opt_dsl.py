b3d8546e5869b5c17e5196d9c0be940901b06ae7
Carsten Portner <cportner@ecehpavw1202a.umd.edu>
2025-04-09 11:56:35 -0400

    Fixed opt_dsl.py

--- hlsfactory/opt_dsl_v2/opt_dsl.py
+++ hlsfactory/opt_dsl_v2/opt_dsl.py
@@ -1,11 +1,12 @@
 import random
 import re
+from collections import defaultdict
 
 class OptDSL:
     def __init__(self, opt_dsl_source: str) -> None:
-        self.pipeline_directives = []
-        self.unroll_directives = []
-        self.partition_directives = []
+        self.pipeline_directives = defaultdict(list)
+        self.unroll_directives = defaultdict(list)
+        self.partition_directives = defaultdict(list)
         self.parse_opt_dsl_lang(opt_dsl_source)
     
     def parse_opt_dsl_lang(self, opt_dsl_source):
@@ -17,19 +18,17 @@ class OptDSL:
         #############################################################################
         exec(opt_dsl_source)
     
-    # TODO Add support for dictionary so no overlapping directives
-
     def pipeline(self, label: str, function: str) -> None:
         directive = f"set_directive_pipeline {function}/{label}"
-        self.pipeline_directives.append(directive)
+        self.pipeline_directives[f"{function}/{label}"].append(directive)
     
     def unroll(self, label: str, function: str, factor: int) -> None:
         directive = f"set_directive_unroll -factor {factor} {function}/{label}"
-        self.unroll_directives.append(directive)
+        self.unroll_directives[f"{function}/{label}"].append(directive)
 
     def partition(self, array_var: str, function: str, partition_type: str, factor: int, dim: int) -> None:
         directive = f"set_directive_array_partition -type {partition_type} -factor {factor} -dim {dim} {function} {array_var}"
-        self.partition_directives.append(directive)
+        self.partition_directives[f"{function}/{array_var}"].append(directive)
     
     def get_directives(self):
         return (self.pipeline_directives,
