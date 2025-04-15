bacaa424704718f25f1389cc029f7895bec703f3
Zhang <mzhang745@ece-linlabsrv01.ece.gatech.edu>
2025-04-14 21:12:31 -0400

    update

--- hlsfactory/hls_dataset_sources/class_ece8893_spring_2025/Mingyu_Zhang_lab3/dataset_hls_ip_export.tcl
+++ hlsfactory/hls_dataset_sources/class_ece8893_spring_2025/Mingyu_Zhang_lab3/dataset_hls_ip_export.tcl
@@ -1,8 +1,6 @@
 open_project project_1
 open_solution "solution1"
 
-# C/RTL co-simulation; can be commented if not needed
-cosim_design
 
 # export generated RTL as an IP; can be commented if not needed
 export_design -format ip_catalog -flow impl
