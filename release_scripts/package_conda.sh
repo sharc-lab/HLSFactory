863c44ec111654f0a60a55162bc57877327b7ab5
Stefan Abi-Karam <stefanabikaram@gmail.com>
2026-09-25 09:12:36 -0400

    fix docs website and remove conda stuff

--- release_scripts/package_conda.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-rattler-build build --recipe ./recipe/meta.yaml --recipe-dir ./recipe --output-dir ./output
-rm -rf output/bld
-rm -rf output/src_cache
-rm -rf output/linux-64
-conda index ./output/
